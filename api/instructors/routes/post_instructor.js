877cfd0c4d01aeaeee4a210c4f2c94c9c409d368
Tamara Temple <tamouse@gmail.com>
2017-03-24 14:58:47 -0500

    2017-03-24T14:58:47

--- api/instructors/routes/post_instructor.js
+++ api/instructors/routes/post_instructor.js
@@ -5,23 +5,26 @@ let instructorsData = require('./../../../data/instructors');
 const payloadValidator = require('./../validation/post_instructor').payloadValidator;
 
 module.exports = {
-  method: 'POST',
-  path: '/api/instructors',
-  config: {
-    pre: [
-      { method: query.verifyUniqueInstructor },
-      { method: query.createInstructorSlug, assign: 'slug' }
-    ],
-    handler: (request, reply) => {
-      let submittedData = request.payload;
-      submittedData.id = instructorsData.length + 1;
-      submittedData.slug = request.pre.slug;
-      instructorsData.push(submittedData);
+    method: 'POST',
+    path: '/api/instructors',
+    config: {
+        validate: {
+            payload: payloadValidator
+        },
+        pre: [
+            { method: query.verifyUniqueInstructor },
+            { method: query.createInstructorSlug, assign: 'slug' }
+        ],
+        handler: (request, reply) => {
+            let submittedData = request.payload;
+            submittedData.id = instructorsData.length + 1;
+            submittedData.slug = request.pre.slug;
+            instructorsData.push(submittedData);
 
-      // The way we respond depends on what we want
-      // to do in the app afterwards
-      // reply({ message: 'Instructor added!' });
-      reply(instructorsData.find(item => item.slug == request.pre.slug));
+            // The way we respond depends on what we want
+            // to do in the app afterwards
+            // reply({ message: 'Instructor added!' });
+            reply(instructorsData.find(item => item.slug == request.pre.slug));
+        }
     }
-  }
 };
--- api/instructors/validation/get_instructors.js
+++ api/instructors/validation/get_instructors.js
@@ -6,7 +6,13 @@ const Joi = require('joi');
 // are 'asc' and 'desc' and for sortKey are 'id',
 // 'name', and 'slug
 const queryValidator = Joi.object({
-  
+    sortDirection: Joi.string().valid(['asc', 'desc']),
+    sortKey: Joi.string().valid(['id', 'name', 'slug']),
+    quah: Joi.string()
 });
 
 module.exports = { queryValidator };
+
+// There's an option to convert strings to upper or lower case if necessary
+
+// The keys on the object passed into Joi.object() is a whitelist of parameters
