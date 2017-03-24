7e38b2322fa1ded518e6f82ecf64cc4fd326f42e
Tamara Temple <tamouse@gmail.com>
2017-03-24 10:28:11 -0500

    2017-03-24T10:28:11

--- server.js
+++ server.js
@@ -3,9 +3,22 @@
 const Hapi = require('hapi');
 
 // Create a Hapi server instance
+const server = new Hapi.Server();
 
 // Tell the server which port to listen on
+server.connection({ port: 3001 });
 
 // Create a hello world route
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (req, res) => {
+        res({ message: "Hello, World!", api: "Hapi" });
+    }
+})
 
 // Start the server and log out the URI
+server.start(err => {
+    if (err) throw err;
+    console.log(`Server listening on port ${server.info.uri}`);
+})
